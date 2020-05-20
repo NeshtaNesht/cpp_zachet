204598cc222db41f9817a7a17a1bd8fd96b3b7bf
NeshtaNesht <dante.pb.b@gmail.com>
2020-05-20 10:08:41 +0500

    next bugfix

--- index.cpp
+++ index.cpp
@@ -27,6 +27,16 @@ const int _GOOD_ = 8; // Кол-во правильных ответов на о
 const int _SATISF_ = 5; // Кол-во правильных ответов на оценку "удовлетворительно"
 const int RND_MAX = 32767;
 
+int genNumber() {
+	auto now = std::chrono::system_clock::now();
+		auto in_time_t = std::chrono::system_clock::to_time_t(now);
+		std::random_device rd;
+			std::mt19937 gen(rd());
+			gen.seed(in_time_t);
+			std::uniform_int_distribution<int> uid(1, _MAX_VALUE_);
+			return uid(gen);
+}
+
 void genVector(std::vector<int>* table, std::vector<int>* table2) {
 	// Сгенерим 10 случайных чисел
 	auto now = std::chrono::system_clock::now();
@@ -57,8 +67,8 @@ int main() {
 		for(int n : answers) {
 			if (n == (first[i-1] * second[i-1])) {
 				// Если совпадения есть - генерируем новые числа
-				first[i-1] = rand() % _MAX_VALUE_;
-				second[i-1] = rand() % _MAX_VALUE_;
+				first[i-1] = genNumber() % _MAX_VALUE_;
+				second[i-1] = genNumber() % _MAX_VALUE_;
 			}
 			// std::cout << "Ответы: " << n << std::endl;
 		}
